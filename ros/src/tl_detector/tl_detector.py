f141d7a08bd681b25adb3fd3383b64536b9e1a9f
Jan Rosicky <jan.rosicky@bohemiasoft.com>
2018-11-19 21:16:09 +0100

    Update of detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import rospy
-from scipy.spatial import KDTree  #kp - for helping in closest waypoint computation
 from std_msgs.msg import Int32
 from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
@@ -8,6 +7,7 @@ from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
+from scipy.spatial import KDTree
 import tf
 import cv2
 import yaml
@@ -22,6 +22,7 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+
         self.waypoints_2d = None
         self.waypoint_tree = None
 
@@ -58,11 +59,10 @@ class TLDetector(object):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        rospy.loginfo ("wpt_cb: invoked")
         self.waypoints = waypoints
-        if not self.waypoints_2d:  #not initialized yet
-            self.waypoints_2d = [ [wpt.pose.pose.position.x, wpt.pose.pose.position.y] for wpt in waypoints.waypoints]
-            self.waypoint_tree = KDTree (self.waypoints_2d)
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -75,25 +75,24 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
-        if (self.waypoint_tree == None):
-            return
-            
         self.has_image = True
         self.camera_image = msg
+        #if self.pose and self.waypoints and self.waypoint_tree :
         light_wp, state = self.process_traffic_lights()
-        #rospy.loginfo ("image_cb: light_wp {0} \n state {1}".format (light_wp, state))
-        
-        """
+
+        '''
         Publish upcoming red lights at camera frequency.
         Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
         of times till we start using it. Otherwise the previous stable state is
         used.
-        """
+        '''
+        #everytime image comes in the call back, determine state
         if self.state != state:
             self.state_count = 0
             self.state = state
         elif self.state_count >= STATE_COUNT_THRESHOLD:
             self.last_state = self.state
+            # if not a red light, keep driving
             light_wp = light_wp if state == TrafficLight.RED else -1
             self.last_wp = light_wp
             self.upcoming_red_light_pub.publish(Int32(light_wp))
@@ -112,9 +111,8 @@ class TLDetector(object):
 
         """
         #TODO implement
-        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
-        # rospy.loginfo("tl_gcwp: x {};  y {} closest_idx {}".format(x, y, closest_idx))
-        return (closest_idx)
+        closest_indx = self.waypoint_tree.query([x,y], 1)[1]
+        return closest_indx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -126,12 +124,12 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # 	for testing just use simulator light state
+
+        #for testing, just return light state
         return light.state
-		
-		#if(not self.has_image):
-        #   self.prev_light_loc = None
-        #    return False
+        #if(not self.has_image):
+            #self.prev_light_loc = None
+            #return False
 
         #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
@@ -148,33 +146,35 @@ class TLDetector(object):
 
         """
         closest_light = None
-        line_wp_idx = None
+        line_wp_indx = None
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if self.pose and self.waypoint_tree:
-            car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
+        if(self.pose):
+            car_wp_indx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
-			#TODO find the closest visible traffic light (if one exists)
+            #TODO find the closest visible traffic light (if one exists)
+            #start with bigger difference
             diff = len(self.waypoints.waypoints)
+
+            #iterate through list of traffic points
             for i, light in enumerate(self.lights):
-                # get the stop light line  way point index
+            #Get stop line waypoint index
                 line = stop_line_positions[i]
-                temp_wp_idx = self.get_closest_waypoint (line[0], line[1])
-                d = temp_wp_idx - car_wp_idx
-                if (d >= 0) and (d < diff):
+                temp_wp_indx = self.get_closest_waypoint(line[0], line[1])
+                # Find closest stop line waypoint index
+                d = temp_wp_indx - car_wp_indx
+                # avoid past waypoints and find closest waypoint index and closest light
+                if d>=0 and d < diff:
                     diff = d
                     closest_light = light
-                    line_wp_idx = temp_wp_idx
+                    line_wp_indx = temp_wp_indx
 
-		if (closest_light):
-			state = self.get_light_state (closest_light)
-			return (line_wp_idx, state)
-		else:
-			#rospy.loginfo ("ptf: unknown state of traffic light")
-			#self.waypoints = None # check on this
-			return -1, TrafficLight.UNKNOWN
-		
+        if closest_light:
+            state = self.get_light_state(closest_light)
+            return line_wp_indx, state
+        #if no traffic light, keep car moving
+        return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
     try:
