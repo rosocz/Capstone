dd8d8d43946ca1c0374a03061e60eebe8bee10d3
komerpoodari <poodari@yahoo.com>
2018-11-26 17:39:46 -0800

    tl detector  updates

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -155,6 +155,7 @@ class TLDetector(object):
         """
         closest_light = None
         line_wp_idx = None
+        
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
